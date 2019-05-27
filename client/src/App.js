b41985a2a96b6fa873614489cef279e639ac0c2b
Rodion Fedotov <fedotov.dev@gmail.com>
2019-05-28 00:24:47 +0500

    signin page & react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+          </Grid>
+          <Grid item xs={8}>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
