95324684c8ea615080940cc2a245c0b6c73f7a3a
Rodion Fedotov <fedotov.dev@gmail.com>
2019-06-12 16:32:07 +0500

    reselect

---
+++ client/src/selectors/index.js
@@ -0,0 +1,11 @@
+export const documents = state => {
+    return state.documents;
+};
+
+export const messages = state => {
+    return state.messages;
+};
+
+export const contacts = state => {
+    return state.contacts;
+};
